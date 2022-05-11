d2a2e55d94690f194aef63ea93715badd2eff74b
elmer718 <elmerfabianrc@ufps.edu.co>
2022-05-11 17:05:04 -0500

    clase conexion forma basica

--- src/co/empresa/test/util/Conexion.java
+++ src/co/empresa/test/util/Conexion.java
@@ -1,5 +1,60 @@
 package co.empresa.test.util;
 
-public class Conexion {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+public class Conexion {
+	private Connection con = null;
+	private PreparedStatement preparedStatement;
+	
+	private static final String url = "jdbc:mysql://localhost:3306";
+	private static final String dbName = "sistema";
+	private static final String driver = "com.mysql.jdbc.Driver";
+	private static final String userName = "root";
+	private static final String password = "";
+	
+	public Conexion() {
+		try {
+			Class.forName(driver).newInstance();
+			con = (Connection)DriverManager.getConnection(url+dbName+userName+password);
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException
+				| SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	
+	public void cerrarConexion() {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	
+	public ResultSet query () throws SQLException{
+		ResultSet res = preparedStatement.executeQuery();
+		return res;
+	}
+	
+	public int execute() throws SQLException{
+		int result = preparedStatement.executeUpdate();
+		return result;
+	}
+	
+	public Connection getCon() {
+		return this.con;
+	}
+	
+	public PreparedStatement setPreparedStatement(String sql) throws SQLException{
+		this.preparedStatement = con.prepareStatement(sql);
+		return this.preparedStatement;
+	}
 }
